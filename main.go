724c6b953588a8f035ae9db97cc4ab9d4913fcb8
PPFD <serj.ermolenko.1861@gmail.com>
2023-01-26 21:52:50 +0300

    Функция пузырьковой сортировки

--- main.go
+++ main.go
@@ -88,6 +88,18 @@ func selectSort(array []int) []int {
 	return array
 }
 
+// Функция реализует пузырьковую сортировку
+func bubbleSort(array []int) []int {
+	for i := 0; i < len(array); i++ {
+		for j := len(array) - 1; j > 0; j-- {
+			if array[j-1] > array[j] {
+				array[j-1], array[j] = array[j], array[j-1]
+			}
+		}
+	}
+	return array
+}
+
 func main() {
 	return
 }
