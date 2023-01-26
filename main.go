7af2dc6ea8ef71ce1d42b27736b64d6b5ceafe09
PPFD <serj.ermolenko.1861@gmail.com>
2023-01-26 21:49:58 +0300

    Функция записи в файл

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -13,6 +16,23 @@ func makePseudoRandArray(amountOfElements int) []int {
 		unsortedArray = append(unsortedArray, rand.Intn(1000))
 	}
 	return unsortedArray
+}
+
+// Записывает в файл с названием FileName, передаваемый массив данных длины amountOfElements
+func writeArrayInFile(array []int, FileName string) {
+	file, err := os.Create(FileName + ".txt")
+
+	if err != nil {
+		fmt.Println("Невозможно создать файл:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	for i := 0; i < len(array); i++ {
+
+		file.WriteString(strconv.Itoa(array[i]))
+		file.WriteString("\n")
+	}
 }
 
 func main() {
