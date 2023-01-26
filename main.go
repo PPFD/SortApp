20eb2015136b87fb8a78c02e34a89b56960e7da0
PPFD <serj.ermolenko.1861@gmail.com>
2023-01-26 21:54:16 +0300

    Пользовательский интерфейс и вызовы функция

--- main.go
+++ main.go
@@ -142,5 +142,149 @@ func QuickSort(array []int) []int {
 }
 
 func main() {
+	for {
+		fmt.Println("Введите количество элементов массива:")
+		sortedArray := []int{}
+		amountOfElements := 0
+		fmt.Scan(&amountOfElements)
+		if amountOfElements > 1 {
+			array := makePseudoRandArray(amountOfElements)
+			writeArrayInFile(array, "unsortedArray")
+			fmt.Println("Массив создан")
+			breakPoint := true  // Условие выхода из бесконечного цикла меню
+			sortedFlag := false // Условие, что массив был отсортирован и можно использовать бинарный поиск
+			for breakPoint == true {
+				fmt.Println("Введите номер действия")
+				fmt.Println("1. Пересоздать массив")
+				fmt.Println("2. Отсортировать массив")
+				fmt.Println("3. Произвести поиск")
+				fmt.Println("0. Завершить работу")
+				action := -1
+				fmt.Scan(&action)
+				switch action {
+				case 1:
+					fmt.Println("Введите количество элементов массива:")
+					fmt.Scan(&amountOfElements)
+					if amountOfElements > 1 {
+						array = makePseudoRandArray(amountOfElements)
+						writeArrayInFile(array, "unsortedArray")
+						fmt.Println("Массив создан")
+						sortedFlag = false
+					} else {
+						fmt.Println("Ошибка, введены неверные данные, повторите попытку")
+					}
+				case 2:
+					fmt.Println("Введите номер действия")
+					fmt.Println("1. Произвести сортировку выбором")
+					fmt.Println("2. Произвести сортировку вставками")
+					fmt.Println("3. Произвести сортировку методом пузырька")
+					fmt.Println("4. Произвести быструю сортировку")
+					fmt.Println("0. Вернуться")
+					action := -1
+					fmt.Scan(&action)
+					switch action {
+					case 1:
+						fmt.Println("Запись начата")
+						sortedArray = selectSort(array)
+						writeArrayInFile(sortedArray, "sortedArray")
+						sortedFlag = true
+						fmt.Println("Запись окончена")
+					case 2:
+						fmt.Println("Запись начата")
+						sortedArray = insertionSort(array)
+						writeArrayInFile(sortedArray, "sortedArray")
+						sortedFlag = true
+						fmt.Println("Запись окончена")
+					case 3:
+						fmt.Println("Запись начата")
+						sortedArray = bubbleSort(array)
+						writeArrayInFile(sortedArray, "sortedArray")
+						sortedFlag = true
+						fmt.Println("Запись окончена")
+					case 4:
+						fmt.Println("Запись начата")
+						sortedArray = QuickSort(array)
+						writeArrayInFile(sortedArray, "sortedArray")
+						sortedFlag = true
+						fmt.Println("Запись окончена")
+					case 0:
+						fmt.Println("Возвращаемся в главное меню")
+						break
+					}
+				case 3:
+					if sortedFlag == true {
+						fmt.Println("Введите номер действия")
+						fmt.Println("1. Произвести линейный поиск в неотсортированном массиве")
+						fmt.Println("2. Произвести бинарный поиск в отсортированном массиве")
+						fmt.Println("0. Вернуться")
+						action := -1
+						fmt.Scan(&action)
+
+						switch action {
+						case 1:
+							fmt.Println("Введите искомый элемент")
+							searchElement := -1
+							fmt.Scan(&searchElement)
+							if int(searchElement) >= 0 {
+								linearSearchUnsorted(array, searchElement)
+							} else if int(searchElement) < 0 {
+								linearSearchUnsorted(array, searchElement)
+							} else {
+								fmt.Println("Введено неверное значение")
+							}
+
+						case 2:
+							fmt.Println("Введите искомый элемент")
+							searchElement := -1
+							fmt.Scan(&searchElement)
+							if int(searchElement) >= 0 {
+								binarySearchSorted(sortedArray, len(array), searchElement)
+							} else if int(searchElement) < 0 {
+								binarySearchSorted(sortedArray, len(array), searchElement)
+							} else {
+								fmt.Println("Введено неверное значение")
+							}
+						case 0:
+							fmt.Println("Возвращаемся в главное меню")
+							break
+						}
+					} else {
+						fmt.Println("Введите номер действия")
+						fmt.Println("Внимание! Созданный массив не был отсортирован, бинарный поиск - недоступен")
+						fmt.Println("1. Произвести линейный поиск в неотсортированном массиве")
+						fmt.Println("0. Вернуться")
+						action := -1
+						fmt.Scan(&action)
+
+						switch action {
+						case 1:
+							fmt.Println("Введите искомый элемент")
+							searchElement := -1
+							fmt.Scan(&searchElement)
+							if int(searchElement) >= 0 {
+								linearSearchUnsorted(array, searchElement)
+							} else if int(searchElement) < 0 {
+								linearSearchUnsorted(array, searchElement)
+							} else {
+								fmt.Println("Введено неверное значение")
+							}
+						case 0:
+							fmt.Println("Возвращаемся в главное меню")
+							break
+						}
+					}
+				case 0:
+					fmt.Println("Завершаю работу")
+					breakPoint = false
+					break
+				}
+			}
+			break
+
+		} else {
+			fmt.Println("Ошибка! Пожалуйста, введите натуральное число элементов массива больше 1")
+		}
+
+	}
 	return
 }
